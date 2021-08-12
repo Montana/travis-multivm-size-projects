8e0a906e21819624bdc9754448ae6033a8b38c3c
Montana Mendy <montana@getprowl.com>
2021-08-12 11:07:48 -0700

    tuple project I created push via vim

---
+++ hello.py
@@ -0,0 +1,12 @@
+# All pair combinations of 2 tuples by Montana Mendy for Travis CI
+  
+test_tuple1 = (4, 5)
+test_tuple2 = (7, 8)
+
+print("The original tuple 1 : " + str(test_tuple1))
+print("The original tuple 2 : " + str(test_tuple2))
+
+res = [(a, b) for a in test_tuple1 for b in test_tuple2]
+res = res + [(a, b) for a in test_tuple2 for b in test_tuple1]
+
+print("The filtered tuple : " + str(res))
